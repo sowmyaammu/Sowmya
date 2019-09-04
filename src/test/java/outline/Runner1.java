d326d61c311bdf9c41d8c511873ae9c8ee898dd9
Training_C2a.04.28 <Training_C2a.04.28@CDC2-D-47B4N62.dir.svc.accenture.com>
2019-09-04 15:38:06 +0530

    hello

---
+++ src/test/java/outline/Runner1.java
@@ -0,0 +1,12 @@
+package outline;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome=true,plugin= {"pretty","json:Myreports1.json"})
+public class Runner1 {
+
+}
